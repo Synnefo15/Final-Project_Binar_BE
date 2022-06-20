55bae9f0ae7aeadbe5f66bc782e54c557202e6df
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-06-20 09:23:00 +0700

    crud transaksi beres

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controller/users.controller');
+const transController = require('../controller/transaksi.controller');
 
 // &---- Routes index ----
 /**
@@ -23,6 +24,13 @@ router.get('/users/profile/:id',userController.findUserByIdApi)
 router.post('/users/profile',userController.createNewUserApi)
 router.put('/users/profile/:id',userController.updateUserApi)
 router.delete('/users/profile/:id',userController.deleteCar)
+
+// &---- Transaksi ----
+router.get('/users/transaksi',transController.findAllTransApi)
+router.get('/users/transaksi/:id',transController.findTransByIdApi)
+router.post('/users/transaksi',transController.createNewTransApi)
+router.put('/users/transaksi/:id',transController.updateTransApi)
+router.delete('/users/transaksi/:id',transController.deleteTrans)
 
 
 

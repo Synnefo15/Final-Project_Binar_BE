efe2b863f4a51b768f5e0eb87060028e55f43e6e
Rafi Putra <gilang20bekek@gmail.com>
2022-06-20 13:55:11 +0000

    Product

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const productController = require('../controller/products.controller.js');
+const categoryController = require('../controller/category.controller.js');
+const cors = require('cors');
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+router.use(cors(corsOptions));
 
 const userController = require('../controller/users.controller');
 
@@ -16,6 +26,20 @@ const userController = require('../controller/users.controller');
 router.get('/', (req, res) => {
 	res.status(200).send(`Default Page`);
 });
+// PRODUCT ENDPOINTS
+router.get('/products', productController.findAllProductsApi);
+router.get('/products/:id', productController.findProductByIdApi);
+router.post('/products', productController.createNewProductApi);
+router.put('/products/:id', productController.updateProductApi);
+router.delete('/products/:id', productController.deleteProduct);
+
+
+// CATEGORY ENDPOINTS
+router.get('/category', categoryController.findAllCategoryApi);
+router.get('/category/:id', categoryController.findCategoryByIdApi);
+router.post('/category', categoryController.createNewCategoryApi);
+router.put('/category/:id', categoryController.updateCategoryApi);
+router.delete('/category/:id', categoryController.deleteCategory);
 
 // &---- User ----
 router.get('/users/profile',userController.findAllUserApi)
@@ -23,6 +47,7 @@ router.get('/users/profile/:id',userController.findUserByIdApi)
 router.post('/users/profile',userController.createNewUserApi)
 router.put('/users/profile/:id',userController.updateUserApi)
 router.delete('/users/profile/:id',userController.deleteCar)
+
 
 
 
